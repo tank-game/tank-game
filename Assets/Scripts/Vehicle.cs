d21fbfcbfb1e1df01a2bc41e21f3477ca5f4b23e
Thomas Freeman <tomclaudefreeman@gmail.com>
2020-06-15 21:09:21 +1000

    Add basic vehicle functionality

---
+++ Assets/Scripts/Vehicle.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class Vehicle : MonoBehaviour
+{
+    public float motorForce;
+    public float steerForce;
+    public float brakeForce;
+
+    public WheelCollider frontLeftWheel, frontRightWheel, rearLeftWheel, rearRightWheel;
+
+    void Update()
+    {
+        float movement = Input.GetAxis("Vertical") * motorForce;
+        float steering = Input.GetAxis("Horizontal") * steerForce;
+        float braking = Input.GetKey(KeyCode.Space) ? brakeForce : 0;
+
+        rearLeftWheel.motorTorque = movement;
+        rearRightWheel.motorTorque = movement;
+
+        frontLeftWheel.steerAngle = steering;
+        frontRightWheel.steerAngle = steering;
+
+        rearLeftWheel.brakeTorque = braking;
+        rearRightWheel.brakeTorque = braking;
+    }
+}
