dcd3a1c72b506cd47ade627846f35b15158e191d
Thomas Freeman <tomclaudefreeman@gmail.com>
2020-06-23 10:30:06 +1000

    Implement turret movement

---
+++ Assets/Scripts/Tank.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class Tank : MonoBehaviour
+{
+    [Header("Movement")]
+    public float motorTorque;
+    public float turnAggression;
+
+    public WheelCollider[] leftWheels, rightWheels;
+
+    void Update()
+    {
+        foreach (WheelCollider wheel in leftWheels)
+        {
+            wheel.motorTorque = Input.GetAxis("Vertical") * motorTorque;
+        }
+
+        foreach (WheelCollider wheel in rightWheels)
+        {
+            wheel.motorTorque = Input.GetAxis("Vertical") * motorTorque;
+        }
+    }
+}
