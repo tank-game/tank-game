3a7331ef6befe0ddbe6a42b0a5193cce9f71bdbf
Thomas Freeman <tomclaudefreeman@gmail.com>
2020-07-20 17:14:53 +1000

    Show visible tank wheels

--- Assets/Scripts/Tank.cs
+++ Assets/Scripts/Tank.cs
@@ -8,20 +8,39 @@ public class Tank : MonoBehaviour
     [Range(5f, 25f)] public float topSpeed; // In metres/second
     [Range(5f, 25f)] public float rotationRate; // In degrees/second
 
+    public Transform wheelModelPrefab;
+
     private Rigidbody rb;
-    private WheelCollider[] wheels;
+
+    private WheelCollider[] wheelColliders;
+    private Transform[] wheelModels;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-        wheels = GetComponentsInChildren<WheelCollider>();
+
+        wheelColliders = GetComponentsInChildren<WheelCollider>();
+        wheelModels = new Transform[wheelColliders.Length];
+        for (int i = 0; i < wheelColliders.Length; i++)
+        {
+            Transform wheelModel = Instantiate(wheelModelPrefab, wheelColliders[i].transform, false);
+            wheelModel.localScale = Vector3.one * wheelColliders[i].radius * 2f;
+            wheelModels[i] = wheelModel;
+        }
     }
 
     public void Move(float movementInput, float rotationInput)
     {
-        foreach (WheelCollider wheel in wheels)
+        for (int i = 0; i < wheelColliders.Length; i++)
         {
+            WheelCollider wheel = wheelColliders[i];
             wheel.motorTorque = rb.velocity.magnitude <= topSpeed ? movementInput * accelerationForce : 0f;
+
+            Vector3 position;
+            Quaternion rotation;
+            wheel.GetWorldPose(out position, out rotation);
+            wheelModels[i].position = position;
+            wheelModels[i].rotation = rotation;
         }
 
         Quaternion deltaRotation = Quaternion.AngleAxis(
